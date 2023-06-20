7da45eec3e20ba9f48410bedc8cb0b57c9f33e28
watermelon36 <106351185+watermelon36@users.noreply.github.com>
2023-06-20 19:52:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function setup(){
 video = createCapture(VIDEO);
 video.size(550,500);
-canvas = createCanvas(550,550);
+canvas = createCanvas(500,500);
 canvas.position(560,170);
 poseNet = ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
